e87fd41fc8e6c56b139dd7dda93c8b171724e715
forloo <kiplingpower6@gmail.com>
2024-07-08 05:49:37 +0900

    Organize code

---
+++ api/constants.tsx
@@ -0,0 +1,1 @@
+export const backgroundOpacity = 0.1;
--- api/index.tsx
+++ api/index.tsx
@@ -3,8 +3,11 @@ import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
 import { handle } from 'frog/vercel'
 import {addUserKey, getStats, userVoted, userValue} from "./vercel_endpoints/requests.js"
-import { createSystem } from 'frog/ui'
+import { createSystem } from 'frog/ui' 
 import { env } from 'node:process';
+import { textComponent, 
+  backgroundImage, outerComponentStyle, 
+  pollResultStatsStyle, textStyle} from './utils.js'
 
 //manually set vercel config variables
 env.KV_REST_API_URL = "https://modest-sawfly-54128.upstash.io"
@@ -36,72 +39,23 @@ app.frame('/', async (c) => {
 
 //route rendering after just voting
 app.frame('/justvoted', async (c) => {
-  const { frameData} = c
-
-  const fid = frameData
-
+  const fid = c.frameData?.fid
   let usrVote : string = "Yes"
   let usrid : undefined | string = undefined
-  
   if (fid){
     usrid = fid.toString()
-    
     const usrvalue = await userValue(usrid)
-
     if (!usrvalue){
       usrVote = "No"
     }
   }
-
   return c.res({
     image: (
       <div
-        style={{
-          alignItems: 'center',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
+        style={outerComponentStyle}
       >
-        <div
-          style={{
-            fontSize: 60,
-            display: 'flex',
-            fontStyle: 'bold',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-
-            position: 'relative', 
-          }}
-        >
-          You have successfully voted "{usrVote}"
-        </div>
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            display: 'flex',
-            height: '100%',
-
-            opacity: 0.3, 
-          }}
-        >
-          <Image
-            objectFit="contain"
-            src="/public/dune.png"
-            width="100%"
-          />
-        </div>
-
+        {textComponent("You have successfully voted" + usrVote)}
+        {backgroundImage()}
       </div>
     ),
     intents: [
@@ -110,19 +64,16 @@ app.frame('/justvoted', async (c) => {
   })
 })
 
+//the voting screen
 app.frame('/pollScreen', async (c) => {
   const { frameData, buttonValue} = c
-
   const fid = frameData
-
   let cur_usr_voted : boolean = false
   let usrid : undefined | string = undefined
-  
   if (fid){
     usrid = fid.toString()
     cur_usr_voted = await userVoted(usrid)
   }
-
   if (buttonValue === "Yes"){
     if (usrid !== undefined){
       await addUserKey(usrid, true);
@@ -132,7 +83,6 @@ app.frame('/pollScreen', async (c) => {
       await addUserKey(usrid, false);
     }
   }
-
   let usrVote = "No";
   if (usrid !== undefined){
     const val = await userValue(usrid!)
@@ -140,58 +90,15 @@ app.frame('/pollScreen', async (c) => {
       usrVote = "Yes";
     }
   }
-
+  const txtToDisplay : string = cur_usr_voted ? 'You already voted: ' + '"' + usrVote +'"'
+  : "There will be over a 10,000 Kramer predictions before 7/29 midnight ";
   return c.res({
     image: (
       <div
-        style={{
-          alignItems: 'center',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
+        style={outerComponentStyle}
       >
-        <div
-          style={{
-            color: 'black',
-            fontSize: 60,
-            display: 'flex',
-            fontStyle: 'bold',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-
-            position: 'relative', 
-          }}
-        >
-          {cur_usr_voted
-            ? 'You already voted: ' + '"' + usrVote +'"'
-            : "There will be over a 10,000 Kramer predictions before 7/29 midnight "}
-        </div>
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            display: 'flex',
-            height: '100%',
-
-            opacity: 0.3,
-          }}
-        >
-          <Image
-            objectFit="contain"
-            src="/public/dune.png"
-            width="100%"
-          />
-        </div>
+        {textComponent(txtToDisplay)}
+        {backgroundImage()}
       </div>
     ),
     intents: cur_usr_voted ? 
@@ -205,113 +112,40 @@ app.frame('/pollScreen', async (c) => {
   })
 })
 
+// result display screen
 app.frame('/resultScreen', async (c) => {
-  const { frameData } = c;
-  const fid = frameData;
-
-
   const [numYes, numNo] = await getStats();
-
-
   const totalVotes = numYes + numNo;
   const yesPercentage = (numYes / totalVotes) * 200 > 0 ? (numYes / totalVotes) * 200 : 500;
   const noPercentage = (numNo / totalVotes) * 200 > 0 ? (numNo / totalVotes) * 200 : 500;
-
   return c.res({
     image: (
-      <div
-        style={{
-          alignItems: 'center',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <Box grow flexDirection="column" gap="8" alignItems="center" alignContent='center' alignSelf='center' textAlign='center'>
-          <div
-            style={{
-              color: 'black',
-              display: 'flex',
-              fontSize: 60,
-              fontStyle: 'bold',
-              letterSpacing: '-0.025em',
-              lineHeight: 1.4,
-              marginTop: 30,
-              padding: '0 120px',
-              whiteSpace: 'pre-wrap',
-            }}
-          >
+      <div style={outerComponentStyle}>
+        <Box 
+          grow 
+          flexDirection="column" 
+          gap="8" 
+          alignItems="center" 
+          alignContent='center' 
+          alignSelf='center' 
+          textAlign='center'
+        >
+          <div style={textStyle}>
             There will be over a 10,000 Kramer predictions before 7/29 midnight
           </div>
           <Box grow flexDirection='row' alignItems='center' marginTop= '30' >
-              <div
-              style={{
-                color: 'white',
-                background: 'darkgreen',
-                fontSize: 60,
-                display: 'flex',
-                fontStyle: 'normal',
-                letterSpacing: '-0.025em',
-                lineHeight: 1.4,
-                marginTop: 30,
-
-                width: yesPercentage,
-                position: 'relative',
-                justifyContent: 'center',
-                opacity: 1,
-                borderRadius: 15,
-              }}
-            >
+            <div style={{...pollResultStatsStyle, width: yesPercentage}}>
               <Box grow flexDirection='row' alignItems='center' justifyContent='center'>
                 {numYes}
               </Box>
-              
             </div>
-            <div
-              style={{
-                color: 'white',
-                backgroundColor: 'darkred',
-                fontSize: 60,
-                display: 'flex',
-                fontStyle: 'normal',
-                letterSpacing: '-0.025em',
-                lineHeight: 1.4,
-                marginTop: 30,
-                whiteSpace: 'pre-wrap',
-                // width: "100%",
-                width: noPercentage,
-                position: 'relative', 
-                borderRadius: 15,
-
-              }}
-            >
+            <div style={{...pollResultStatsStyle, width: noPercentage}}>
               <Box grow flexDirection='row' alignItems='center' justifyContent='center'>
                 {numNo}
               </Box>
             </div>
           </Box>
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              display:'flex',
-              height: '100%',
-              width: '100%',
-
-              opacity: 0.3, 
-            }}
-          >
-            <Image
-            objectFit="contain"
-            src="/public/dune.png"
-            width="100%"
-          />
-          </div>
+          {backgroundImage()}
         </Box>
       </div>
     ),
